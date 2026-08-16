fdede87cfd4e8873cefb8957fd4782e1f10e615c
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-16 12:40:34 +0200

    Android: preserve NestedScrollView fling nested-scroll lifecycle

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactNestedScrollView.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactNestedScrollView.kt
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<cf60b52e15df339a179f392723007fab>>
+ * @generated SignedSource<<a70eb9d9a6016b8210d8f2603806c222>>
  */
 
 /**
@@ -699,21 +699,6 @@ constructor(context: Context, private val fpsListener: FpsListener? = null) :
 
     if (pagingEnabled) {
       flingAndSnap(correctedVelocityY)
-    } else if (scroller != null) {
-      val scrollWindowHeight = height - paddingBottom - paddingTop
-      scroller.fling(
-          scrollX, // startX
-          scrollY, // startY
-          0, // velocityX
-          correctedVelocityY, // velocityY
-          0, // minX
-          0, // maxX
-          0, // minY
-          Int.MAX_VALUE, // maxY
-          0, // overX
-          scrollWindowHeight / 2, // overY
-      )
-      postInvalidateOnAnimation()
     } else {
       super.fling(correctedVelocityY)
     }

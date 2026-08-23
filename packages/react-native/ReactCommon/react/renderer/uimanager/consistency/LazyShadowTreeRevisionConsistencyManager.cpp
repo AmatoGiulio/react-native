7cf78db21b765ad53ab326aad6fc688a3fcc08b2
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-23 15:11:59 +0200

    refactor: keep shadow tree revision provider committed-only

--- packages/react-native/ReactCommon/react/renderer/uimanager/consistency/LazyShadowTreeRevisionConsistencyManager.cpp
+++ packages/react-native/ReactCommon/react/renderer/uimanager/consistency/LazyShadowTreeRevisionConsistencyManager.cpp
@@ -6,128 +6,9 @@
  */
 
 #include "LazyShadowTreeRevisionConsistencyManager.h"
-
-#include <folly/json.h>
 #include <glog/logging.h>
-#include <react/renderer/core/ComponentDescriptor.h>
-#include <react/renderer/core/LayoutableShadowNode.h>
-#include <react/renderer/core/PropsParserContext.h>
-#include <react/renderer/core/RawProps.h>
-#include <react/renderer/core/ShadowNodeFragment.h>
-#include <react/renderer/uimanager/consistency/PresentationPropsRegistry.h>
-
-#include <unordered_map>
-#include <unordered_set>
 
 namespace facebook::react {
-
-namespace {
-
-struct ResolvedPresentedPropsSnapshot {
-  std::shared_ptr<const ShadowNodeFamily> family;
-  folly::dynamic props;
-};
-
-int& transformProbeCount() {
-  static int value = 0;
-  return value;
-}
-
-RootShadowNode::Shared applyPresentationProps(
-    const RootShadowNode::Shared& committedRoot) {
-  if (committedRoot == nullptr) {
-    return nullptr;
-  }
-
-  auto snapshots =
-      PresentationPropsRegistry::get(committedRoot->getSurfaceId());
-  if (snapshots.empty()) {
-    return committedRoot;
-  }
-
-  std::unordered_set<std::shared_ptr<const ShadowNodeFamily>> families;
-  std::unordered_map<Tag, ResolvedPresentedPropsSnapshot> resolvedSnapshots;
-  for (const auto& [tag, snapshot] : snapshots) {
-    auto family = snapshot.family.lock();
-    if (family == nullptr) {
-      continue;
-    }
-
-    auto ancestors = family->getAncestors(*committedRoot);
-    if (!ancestors.empty()) {
-      families.insert(family);
-      resolvedSnapshots.emplace(
-          tag,
-          ResolvedPresentedPropsSnapshot{
-              .family = std::move(family),
-              .props = snapshot.props,
-          });
-    }
-  }
-
-  if (families.empty()) {
-    return committedRoot;
-  }
-
-  auto presentedRoot = committedRoot->cloneMultiple(
-      families,
-      [&resolvedSnapshots](
-          const ShadowNode& shadowNode,
-          const ShadowNodeFragment& fragment) {
-        auto newProps = ShadowNodeFragment::propsPlaceholder();
-        auto snapshotIt = resolvedSnapshots.find(shadowNode.getTag());
-        const bool hasPresentedSnapshot =
-            snapshotIt != resolvedSnapshots.end() &&
-            snapshotIt->second.family == shadowNode.getFamilyShared();
-        if (hasPresentedSnapshot) {
-          PropsParserContext propsParserContext{
-              shadowNode.getSurfaceId(), *shadowNode.getContextContainer()};
-          newProps = shadowNode.getComponentDescriptor().cloneProps(
-              propsParserContext,
-              shadowNode.getProps(),
-              RawProps(snapshotIt->second.props));
-        }
-
-        auto clonedNode = shadowNode.clone(
-            {.props = newProps,
-             .children = fragment.children,
-             .state = shadowNode.getState()});
-
-        if (hasPresentedSnapshot && transformProbeCount() < 8) {
-          const auto* committedLayoutable =
-              dynamic_cast<const LayoutableShadowNode*>(&shadowNode);
-          const auto* clonedLayoutable =
-              dynamic_cast<const LayoutableShadowNode*>(clonedNode.get());
-          if (committedLayoutable != nullptr && clonedLayoutable != nullptr) {
-            const auto committedTransform = committedLayoutable->getTransform();
-            const auto clonedTransform = clonedLayoutable->getTransform();
-            LOG(INFO) << "[PG_PATCH] clone-probe tag=" << shadowNode.getTag()
-                      << " raw=" << folly::toJson(snapshotIt->second.props)
-                      << " committedY=" << committedTransform.matrix[13]
-                      << " clonedY=" << clonedTransform.matrix[13];
-            transformProbeCount()++;
-          }
-        }
-
-        return clonedNode;
-      });
-
-  if (presentedRoot == nullptr) {
-    return committedRoot;
-  }
-
-  static bool didLogPresentedRoot = false;
-  if (!didLogPresentedRoot) {
-    didLogPresentedRoot = true;
-    LOG(INFO) << "[PG_PATCH] root-built surface="
-              << committedRoot->getSurfaceId()
-              << " families=" << families.size();
-  }
-
-  return std::static_pointer_cast<RootShadowNode>(presentedRoot);
-}
-
-} // namespace
 
 LazyShadowTreeRevisionConsistencyManager::
     LazyShadowTreeRevisionConsistencyManager(
@@ -148,20 +29,15 @@ LazyShadowTreeRevisionConsistencyManager::updateCurrentRevision(
         reactRevision.value_or(shadowTree.getCurrentRevision()).rootShadowNode;
   });
 
-  auto visibleRootShadowNode = applyPresentationProps(rootShadowNode);
-
   std::unique_lock lock(capturedRootShadowNodesForConsistencyMutex_);
 
   // We don't need to store the revision if we haven't locked.
-  // We can resolve lazily when requested. When locked, capture both committed
-  // state and the current presentation overlay once so all reads in the same
-  // JS task observe the same revision.
+  // We can resolve lazily when requested.
   if (lockCount > 0) {
-    capturedRootShadowNodesForConsistency_[surfaceId] =
-        visibleRootShadowNode;
+    capturedRootShadowNodesForConsistency_[surfaceId] = rootShadowNode;
   }
 
-  return visibleRootShadowNode;
+  return rootShadowNode;
 }
 
 #pragma mark - ShadowTreeRevisionProvider
